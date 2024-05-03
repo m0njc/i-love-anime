425eb9b36af45022b0c088ad7ef1f1d85880f923
Monica Lam <mondanngar@gmail.com>
2024-05-03 17:28:53 +1000

    wip: adding apollo client and draft query

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -1,8 +1,37 @@
-'use client'
-
 import ContentPage from "@/components/page/content";
 
-export default function Content() {
+import { getClient } from "@/lib/apolloClient";
+import { gql } from "@apollo/client";
+
+export default async function Content() {
+
+    const query = gql`query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+        Page (page: $page, perPage: $perPage) {
+          pageInfo {
+            total
+            currentPage
+            lastPage
+            hasNextPage
+            perPage
+          }
+          media (id: $id, search: $search) {
+            id
+            title {
+              english
+            }
+            description
+            status
+            coverImage {
+              extraLarge
+              large
+              medium
+              color
+            }
+            siteUrl
+          }
+        }
+      }`;
+    const { data } = await getClient().query({ query });
 
   return (
     <ContentPage />
